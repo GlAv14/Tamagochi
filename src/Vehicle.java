e3414f0abec0b354983f29c9cbc96776c6433d13
GlebWorkingPc <averyanov1411@icloud.com>
2025-06-17 14:39:11 +0300

    Override method toString

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,13 +1,14 @@
 public class Vehicle {
     public static void main(String[] args) {
         Vehicle[] vehicles = new Vehicle[4];
-        vehicles[0] = new Car("Toyota","Camry",2020,4);
-        vehicles[1] = new Car("BMW","X5",2012,4);
-        vehicles[2] = new Motorcycle("Kawasaki","Ninja",2018,"Sport");
-        vehicles[3] = new Motorcycle("Harley Davidson","King",2005,"Cruiser");
+        vehicles[0] = new Car("Toyota", "Camry", 2020, 4);
+        vehicles[1] = new Car("BMW", "X5", 2012, 4);
+        vehicles[2] = new Motorcycle("Kawasaki", "Ninja", 2018, "Sport");
+        vehicles[3] = new Motorcycle("Harley Davidson", "King", 2005, "Cruiser");
 
-        for (Vehicle vehicle: vehicles){
-            System.out.println(vehicle);
+        for (Vehicle vehicle : vehicles) {
+            System.out.println(vehicle.toString());
+            System.out.println();
         }
 
     }
@@ -28,6 +29,13 @@ public class Vehicle {
         System.out.println("Model: " + model);
         System.out.println("Year: " + year);
     }
+
+    public String toString() {
+        return "Vehicle Info:" + "\n" +
+                "Brand: " + brand + "\n" +
+                "Model: " + model + "\n" +
+                "Year: " + year+"\n";
+    }
 }
 
 class Car extends Vehicle {
@@ -36,6 +44,9 @@ class Car extends Vehicle {
     public Car(String brand, String model, int year, int numberOfDoors) {
         super(brand, model, year);
         this.numberOfDoors = numberOfDoors;
+    }
+    public String toString(){
+        return super.toString()+ "Number of doors: " + numberOfDoors;
     }
 
     @Override
@@ -49,9 +60,12 @@ class Car extends Vehicle {
 class Motorcycle extends Vehicle {
     String typeOfHandlebar;
 
-    public Motorcycle(String brand,String model,int year,String typeOfHandlebar) {
+    public Motorcycle(String brand, String model, int year, String typeOfHandlebar) {
         super(brand, model, year);
         this.typeOfHandlebar = typeOfHandlebar;
+    }
+    public String toString(){
+        return  super.toString()+ "Type of Handlebar: " + typeOfHandlebar;
     }
 
     @Override
